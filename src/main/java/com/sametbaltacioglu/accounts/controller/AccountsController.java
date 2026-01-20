6c3f1cf35eb7f6fd2c2a6fd98f641b8dd01d4d11
smtbb <sametbaltacioglu34@gmail.com>
2026-01-20 20:32:04 +0300

    update

--- src/main/java/com/sametbaltacioglu/accounts/controller/AccountsController.java
+++ src/main/java/com/sametbaltacioglu/accounts/controller/AccountsController.java
@@ -1,13 +1,19 @@
 package com.sametbaltacioglu.accounts.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sametbaltacioglu.accounts.dto.CustomerDto;
+import com.sametbaltacioglu.accounts.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class AccountsController {
 
-    @GetMapping("sayHello")
-    public String sayHello() {
-        return "Hello Mal rümük!";
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDto());
     }
 }
